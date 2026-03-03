5ac06e6f12cb97bae99a096558e43ab1b2970cb9
OMI-Systems <omar.perez.nieva@gmail.com>
2026-03-02 21:15:49 -0600

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,20 @@
+window.SITE_CONFIG = {
+  brand: {
+    name: "Peceras & Muebles Puebla",
+    shortMark: "PM", // 2 letras
+  },
+  location: {
+    city: "Puebla",
+    coverage: "Puebla y alrededores",
+  },
+  whatsapp: {
+    number: "522212810388",
+    message:
+      "Hola, quiero una cotización. Busco: Pecera / Mueble / Mantenimiento. Estoy en: _. Medidas: __. Presupuesto: ___.",
+  },
+  seo: {
+    title: "Peceras & Muebles Puebla | Peceras personalizadas y muebles a medida",
+    description:
+      "Peceras personalizadas, muebles a medida, instalación y mantenimiento en Puebla y alrededores. Cotiza por WhatsApp.",
+  },
+};
